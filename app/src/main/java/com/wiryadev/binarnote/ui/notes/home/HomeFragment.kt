7860e6761d04ea01ec68f1efb2960dae0622aa9a
wiryadev <abrarwiryawan@gmail.com>
2022-03-31 19:27:52 +0700

    add bottom sheet dialog

--- app/src/main/java/com/wiryadev/binarnote/ui/notes/home/HomeFragment.kt
+++ app/src/main/java/com/wiryadev/binarnote/ui/notes/home/HomeFragment.kt
@@ -1,12 +1,14 @@
 package com.wiryadev.binarnote.ui.notes.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.wiryadev.binarnote.R
 import com.wiryadev.binarnote.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +18,8 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     private val viewModel: HomeViewModel by viewModels()
 
@@ -30,6 +34,10 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet.root)
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
         viewModel.userSession.observe(viewLifecycleOwner) {
             if (it.email.isBlank()) {
                 findNavController().navigate(
@@ -41,11 +49,28 @@ class HomeFragment : Fragment() {
         binding.btnLogout.setOnClickListener {
             viewModel.logout()
         }
+
+        with(binding) {
+            fabAddNote.setOnClickListener {
+                bottomSheet.tvTitle.text = getString(R.string.add_new_note)
+                bottomSheet.btnInput.text = getString(R.string.create_note)
+
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                    fabAddNote.setImageResource(R.drawable.ic_round_add_24)
+                } else {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                    fabAddNote.setImageResource(R.drawable.ic_round_close_24)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 
 }
